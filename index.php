148c90070b73f46efa899d439eed559df4212448
pedzed <pedzed@outlook.com>
2016-05-19 17:26:41 +0200

    Added files.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <title>Website Title</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/screen.css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+
+</head>
+<body>
+    <header>
+        <h1 class="logo">Logo</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="downloads.php">Downloads</a></li>
+                <li><a href="forum.php">Forum</a></li>
+                <li><a href="contact.php">Contact us</a></li>
+            </ul>
+        </nav>
+        <div class="clear"></div>
+    </header>
+    <div id="container">
+
+        <aside id="Just_A_Random_ID">
+            <div class="widget">
+                <h2>Widget Header</h2>
+                <div class="inner">
+                    Widget contents
+                </div>
+            </div>
+        </aside>
+      <h1>Home</h1>
+      <p>Just a template.</p>
+    </div>
+    <footer>
+        &copy; phpacademy.org 2011. All rights reserved.
+    </footer>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
